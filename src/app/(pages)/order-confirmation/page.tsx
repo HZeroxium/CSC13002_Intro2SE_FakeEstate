a5a38a8278e1c9fcc5db4e561443842f5b3cf062
Nguyen Gia Huy <134088678+HZeroxium@users.noreply.github.com>
2024-07-29 20:11:24 +0700

    order-confirmation: update OrderConfirmationPage component

--- src/app/(pages)/order-confirmation/page.tsx
+++ src/app/(pages)/order-confirmation/page.tsx
@@ -1,11 +1,8 @@
-// #include from "./FakeEstate/node_modules/@types/..."
 import React, { Suspense } from 'react'
-
-// #include from "./FakeEstate/node_modules/..."
 import { Metadata } from 'next'
 
-import { Gutter } from '../../../app/_components/Gutter'
-import { mergeOpenGraph } from '../../../app/_utilities/mergeOpenGraph'
+import { Gutter } from '../../_components/Gutter'
+import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { OrderConfirmationPage } from './OrderConfirmationPage'
 
 import classes from './index.module.scss'
