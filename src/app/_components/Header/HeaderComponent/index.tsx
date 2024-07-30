9d0cbe04803e17d93113dc4b41c69838198becf5
Nguyen Gia Huy <134088678+HZeroxium@users.noreply.github.com>
2024-07-30 11:24:19 +0700

    components/Header: refactor, simplify and fix import errors

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -2,18 +2,16 @@
 
 // #include from "./FakeEstate/node_modules/@types/..."
 import React from 'react'
-
+import Image from 'next/image'
 // #include from "./FakeEstate/node_modules/..."
 import Link from 'next/link'
-import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 
-// #include from "./FakeEstate/src/..."
-import { Header } from '../../../../payload/payload-types'
-import { noHeaderFooterUrls } from '../../../../app/constants'
 import { Gutter } from '../../../../app/_components/Gutter'
 import { HeaderNav } from '../../../../app/_components/Header/Nav'
-import SearchComponent from '../../../../app/_components/Header/SeachComponent'
+import { noHeaderFooterUrls } from '../../../../app/constants'
+// #include from "./FakeEstate/src/..."
+import { Header } from '../../../../payload/payload-types'
 
 // #include css from "./FakeEstate/src/app/_components/Header/HeaderComponent/..."
 import classes from './index.module.scss'
@@ -22,18 +20,28 @@ const HeaderComponent = ({ header }: { header: Header }) => {
   const pathname = usePathname()
 
   return (
-    <nav className = {[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide].filter(Boolean).join(' ')}>
-      <Gutter className = {classes.wrap}>
-        <HeaderNav header = {header}/>
-      </Gutter> 
-      <Gutter className={classes.wrap_search}>
-        <h1>Stride Towards A <br /> Digital Tomorrow</h1>
-        <form action="" className={classes.searchBox}>
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image src="fakeestate.svg" alt="logo" width={170} height={50} />
+        </Link>
+        <HeaderNav header={header} />
+        {/* <form action="" className={classes.searchBox}>
           <input type="text" className={classes.searchText} />
           <button className={classes.searchBtn}>
-            <Image className={classes.searchIcon} src="/assets/icons/magnifying-glass-solid-white.svg" alt="search-btn" width={24} height={24} />
+            <Image
+              className={classes.searchIcon}
+              src="/assets/icons/magnifying-glass-solid-white.svg"
+              alt="search-btn"
+              width={24}
+              height={24}
+            />
           </button>
-        </form>
+        </form> */}
       </Gutter>
     </nav>
   )
