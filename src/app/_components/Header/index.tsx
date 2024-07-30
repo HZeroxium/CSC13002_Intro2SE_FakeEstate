4a14aaae3c13eb7f24b800dfc8b3a67ef702b17e
Nguyen Gia Huy <134088678+HZeroxium@users.noreply.github.com>
2024-07-30 11:24:34 +0700

    components/Header: fix import errors

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -6,14 +6,13 @@
 
 // #include from "./FakeEstate/node_modules/@types/..."
 import React from 'react'
-
 // #include from "./FakeEstate/node_modules/..."
 import Link from 'next/link'
 
-// #include from "./FakeEstate/src/..."
-import { Header } from '../../../payload/payload-types'
 import { fetchHeader } from '../../../app/_api/fetchGlobals'
 import HeaderComponent from '../../../app/_components/Header/HeaderComponent'
+// #include from "./FakeEstate/src/..."
+import type { Header } from '../../../payload/payload-types'
 
 export async function Header() {
   let header: Header | null = null
